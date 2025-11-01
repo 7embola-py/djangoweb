1fa2953f725432ac63e580df2dd6dfbb6cf9dd48
7embola-py <yahyamorchid64@gmail.com>
2025-11-01 22:39:29 +0100

    Initial commit: LibraryProject setup

--- PycharmProjects/Python4Github/Alx_DjangoLearnLab/Introduction_to_Django/LibraryProject/settings.py
+++ PycharmProjects/Python4Github/Alx_DjangoLearnLab/Introduction_to_Django/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-huut&01doop!!#jon1h+^+#h&@u%fu0fm3r*bd)!7d7ar)3g53'
+SECRET_KEY = 'django-insecure-!+_ny%5!%9ynq-j&qjqw9hywv*hymzmw+r^id%7g#(xo59q0s-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
